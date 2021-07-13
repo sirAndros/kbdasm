fac45282a09adb1774fde3330729d3298480d10d
sir_Andros <andros.osipkov@gmail.com>
2021-07-13 18:09:26 +0200

    ref: move `get-scancodes` to `utils`. Remove `kana-led`

--- kana_led/make.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-gcc -m32 -Wall -O2 -fno-ident -fomit-frame-pointer -fno-exceptions -fno-asynchronous-unwind-tables -flto -s kana_led.c -nostdlib -lkernel32 -luser32 -lshell32 -lmsvcrt -Wl,-e_xMain -mwindows
