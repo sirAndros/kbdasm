a7014378e8a2fe704d4ca19b73f7fa6e7fd56356
sir_Andros <andros.osipkov@gmail.com>
2021-07-13 13:14:20 +0200

    fix: build task

--- make.bat
+++ make.bat
@@ -3,6 +3,7 @@ set include=%~pd0fasm\include
 set arch=x86
 if exist "%programfiles(x86)%" set arch=AMD64
 if "%1"=="" goto:make
+if exist "%~n1.dll" del "%~n1.dll"
 "%~pd0fasm\fasm.exe" %*
 goto:eof
 :make
